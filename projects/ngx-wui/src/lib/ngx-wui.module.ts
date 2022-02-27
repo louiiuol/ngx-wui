785cfe5d9c55f39e63c536df7b7428c328a3b63b
louiiuol <louis.godlewski@vidmizer.com>
2022-02-27 18:54:07 +0100

    Adding angular library

---
+++ projects/ngx-wui/src/lib/ngx-wui.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+const components: any[] = [];
+
+@NgModule({
+  declarations: [...components],
+  imports: [
+  ],
+  exports: [...components]
+})
+export class NgxWuiModule { }
---
+++ projects/ngx-wui/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-wui
+ */
+
+export * from './lib/ngx-wui.service';
+export * from './lib/ngx-wui.component';
+export * from './lib/ngx-wui.module';
